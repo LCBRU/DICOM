7ce3d035d6f9b5330391d3232973baf00b80544e
DanLawday <rlawday@hotmail.com>
2021-09-07 16:03:41 +0100

    program drivering almost done

--- OpenASearch.py
+++ OpenASearch.py
@@ -1,8 +1,8 @@
 import pyautogui
 import requests, re
-#import httplib2
-#import urllib
-#import scrapy
+# import httplib2
+# import urllib
+# import scrapy
 from time import sleep
 from datetime import *
 from selenium import webdriver
@@ -17,32 +17,34 @@ from bs4 import BeautifulSoup
 import pyodbc
 import pandas as pd
 import os
+import subprocess
+from pywinauto import Desktop,Application
 from pathlib import Path
+
 conn = pyodbc.connect('Driver={SQL Server};'
                       r'Server=UHLSQLPRIME01\UHLBRICCSDB;'
                       'Database=i2b2_app03_b1_data;'
                       'Trusted_Connection=yes;')
 
 ########################### Build list of (['UhlSystemNumber', 'MRN', 'BptNumber', 'RecruitingSite', 'ct_count','echo_count', 'DICOM_Images_Pseudonymised', 'ct_date_time_start'])
-ListToDicom = pd.read_sql_query('SELECT * FROM i2b2_app03_b1_data.dbo.DICOM_List',conn)
+ListToDicom = pd.read_sql_query('SELECT * FROM i2b2_app03_b1_data.dbo.DICOM_List', conn)
 print(ListToDicom.columns)
 print(ListToDicom.head(1))
 print('How may outstanting?')
 print(ListToDicom.index.max())
 
-
-#select the search button
+# select the search button
 driver = webdriver.Ie()
 driver.implicitly_wait(3)
 driver.get("https://uvweb.pacs.uhl-tr.nhs.uk/login.jsp")
 driver.maximize_window()
 
-#LogMeIn
+# LogMeIn
 #  .env
 
 f = open("G:\dicom\.env", "r")
-myid =f.readline().splitlines()
-mypass =f.readline()
+myid = f.readline().splitlines()
+mypass = f.readline()
 f.close()
 myid
 mypass
@@ -56,22 +58,23 @@ password.send_keys(mypass)
 
 login = driver.find_element_by_name("login")
 login.click()
-#end of LogMeIn
+# end of LogMeIn
 
 PacsWindow = driver.window_handles
 print(PacsWindow)
 
 ################ start iterations
-i = 8
+i = 9
 NextInList = ListToDicom.at[i, 'MRN']
-#NextInList = 'RWES0112807'   ############################HARD CODED FOR TESTING, REMOVE TO GO LIVE
+# NextInList = 'RWES0112807'   ############################HARD CODED FOR TESTING, REMOVE TO GO LIVE
 NextInList_bpt = ListToDicom.at[i, 'BptNumber']
 
-date_to_find = datetime.utcfromtimestamp(ListToDicom['ct_date_time_start'].values[i].astype(datetime)/1_000_000_000).strftime('%m-%d-%Y')
+date_to_find = datetime.utcfromtimestamp(
+    ListToDicom['ct_date_time_start'].values[i].astype(datetime) / 1_000_000_000).strftime('%m-%d-%Y')
 print(NextInList)
 print(date_to_find)
 
-#open advanced search and find its handle and switch to window
+# open advanced search and find its handle and switch to window
 
 PacsWindow = driver.window_handles
 print(PacsWindow)
@@ -85,15 +88,16 @@ advsearchwindow = list(set(driver.window_handles) - set(PacsWindow))[0]
 driver.window_handles
 print(advsearchwindow)
 print(PacsWindow)
-driver.switch_to.window(advsearchwindow) #driver.switch_to.window(PacsWindow) #driver.switch_to.window(advsearchwindow)
+driver.switch_to.window(
+    advsearchwindow)  # driver.switch_to.window(PacsWindow) #driver.switch_to.window(advsearchwindow)
 driver.switch_to.frame("LIST")
 
 ########## Enter details for search and submit
 sleep(1)
 driver.find_element_by_name("searchPatientId").send_keys(NextInList)
-driver.find_element_by_name("searchFirstName").click() # click away from searchPatientId to enable searchStudyDescr
-#driver.execute_script("dialogForm.searchOrderStatus[8].click();") # sets to complete, however we mhy also want read offline...
-driver.execute_script("dialogForm.searchImgCnt.click();") #only studys with images
+driver.find_element_by_name("searchFirstName").click()  # click away from searchPatientId to enable searchStudyDescr
+# driver.execute_script("dialogForm.searchOrderStatus[8].click();") # sets to complete, however we mhy also want read offline...
+driver.execute_script("dialogForm.searchImgCnt.click();")  # only studys with images
 driver.find_element_by_name("searchStudyDescr").send_keys("CT Cardiac angiogram")
 
 driver.switch_to.window(advsearchwindow)
@@ -101,7 +105,7 @@ driver.switch_to.frame("TOOLBAR")
 butts = driver.find_elements_by_class_name("search_button")
 butts[0].click()
 sleep(3)
-#driver.window_handles by monitoring the window_handles it seems to close the connection!!!!!!!!!!
+# driver.window_handles by monitoring the window_handles it seems to close the connection!!!!!!!!!!
 print(PacsWindow)
 print(driver.window_handles)
 
@@ -109,16 +113,16 @@ print("switching...PacsWindow")
 driver.switch_to.window(PacsWindow[0])
 driver.switch_to.frame("tableFrame")
 
-soup = BeautifulSoup(driver.page_source,'lxml')
-pprint(soup.find('td', string = re.compile(date_to_find)))
-number_of_Dicoms_on_right_Date = len(soup.find_all('td', string = re.compile(date_to_find)))
+soup = BeautifulSoup(driver.page_source, 'lxml')
+pprint(soup.find('td', string=re.compile(date_to_find)))
+number_of_Dicoms_on_right_Date = len(soup.find_all('td', string=re.compile(date_to_find)))
 print('number of Dicoms on date of intrest')
 print(number_of_Dicoms_on_right_Date)
 # Now to record the number of in range (should only be 1 for yes or 0 for nune however it's possible there are
 # more then one.
 
 # if there is only valid one in the list, create new folder and Select
-if number_of_Dicoms_on_right_Date==1:
+if number_of_Dicoms_on_right_Date == 1:
     path = os.path.join('C:\\briccs_ct\\', NextInList_bpt)
     print("folder to save is")
     print(path)
@@ -127,7 +131,8 @@ if number_of_Dicoms_on_right_Date==1:
     listTableForm = driver.find_element_by_name("listTableForm")
     sleep(1)
     listTableForm.click()
-to_log = np.array([NextInList + ',' + NextInList_bpt + ',' + str(number_of_Dicoms_on_right_Date) + ',' + str(datetime.now())])
+to_log = np.array(
+    [NextInList + ',' + NextInList_bpt + ',' + str(number_of_Dicoms_on_right_Date) + ',' + str(datetime.now())])
 print(to_log)
 with open("C:\\briccs_ct\\results.csv", "ab") as f:
     np.savetxt(f, (to_log), fmt='%s', delimiter=' ')
@@ -136,30 +141,38 @@ with open("C:\\briccs_ct\\results.csv", "ab") as f:
 # ----- pyautogui.rightClick() and the pointer can be just about anywhere, sending F12 would be better
 # ----- pyautogui.press('f12') works
 
-sleep(5)
-pyautogui.press('f12')
-pyautogui.typewrite('j')
-pyautogui.typewrite('j')
-pyautogui.typewrite('j')
-pyautogui.typewrite('j')
-pyautogui.typewrite(path)
-pyautogui.typewrite('j')
-pyautogui.typewrite('j')
-pyautogui.typewrite('down')
-sleep(5)
-print("trying exit.jpg")
-pyautogui.click('exit.jpg')
-print("failed?")
+cmd = 'WMIC PROCESS get Caption,Commandline,Processid'
+proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+for line in proc.stdout:
+    print(line)
 
 
 ########## Export the image
-# sleep(6) # expect a the page to take a while to load.
-#select using win32 the other program window
-#left click
-#entire study
-#file type DICOM(*.dcm)
-#option - Anonymise checked
-#Root folder to be bptnumber
+sleep(5)  # page loading
+pyautogui.press('f12')      # opens the save images windows, focused on save button by default
+pyautogui.typewrite('d')    # set file type to DICOM(*.dcm)
+pyautogui.press('tab')      # tab over to folder
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.typewrite(path)   # set to predefined path
+pyautogui.press('tab')      # tab apply to images
+pyautogui.press('tab')
+pyautogui.press('down') # select the non-default save 'Entire Study' (two down from default)
+pyautogui.press('down')
+pyautogui.press('tab')      # tab to do not create subfolder
+pyautogui.press('space')           # click not create subfolder
+pyautogui.press('tab')      # select File Name Header and type bpt number in
+pyautogui.typewrite(NextInList_bpt)
+                            # option - anonymise should always be defult and checked
+pyautogui.press('tab')      # tab to the save button
+pyautogui.press('space')    # time to save the files!
+sleep(600)                  # take about ten minutes to save the stuff to C drive
+
+#pyautogui.click('exit.jpg') # need to exit now, not sure how to....
+print("fin")
+
+# Root folder to be bptnumber
 # sleep(600) # 10 expect a ten minute download time.
 # find icon (done).Click
 ##########
@@ -168,66 +181,56 @@ print("failed?")
 # END Start again with next in set!
 
 
-
-
-
-
-
-
-
-
-
-
 ###################################################
 #################     Apendix     #################
 ###################################################
-#print(soup.prettify())
-#driver.find_element_by_id("id0").click() #open Study results
+# print(soup.prettify())
+# driver.find_element_by_id("id0").click() #open Study results
 # Ref. Phys <> DO NOT USE - Referrer must be specified
 
-#qsearchstring = \
-#driver.find_element_by_name("qsearchstring").send_keys(NextInList)
-#qsearchbutton = driver.find_element_by_name("qsearch")
-#qsearchstring.clear()
-#qsearchstring
-#qsearchbutton.click()
-#note at this point qsearchstring and qsearchbutton will now be out of date.
+# qsearchstring = \
+# driver.find_element_by_name("qsearchstring").send_keys(NextInList)
+# qsearchbutton = driver.find_element_by_name("qsearch")
+# qsearchstring.clear()
+# qsearchstring
+# qsearchbutton.click()
+# note at this point qsearchstring and qsearchbutton will now be out of date.
 
 
-#advsearch = driver.find_element_by_id("search")
-#search_button_map = driver.find_element_by_name("search_button_map")
+# advsearch = driver.find_element_by_id("search")
+# search_button_map = driver.find_element_by_name("search_button_map")
 
-#select = driver.find_element_by_name("select")
-#search.click()
-#search.is_displayed()
-#search.submit()
-#search.send_keys("openSearch(True);")
-#QuickSearch = driver.find_element_by_xpath("//img[@title='Quick Search']")
-#search = driver.find_element_by_xpath("//img[@id='search']")
-#search = driver.find_element_by_xpath("//img[@name='search']")
-#search = driver.find_element_by_xpath("//img[@alt='Search']")
-#driver.switch_to.default_content()
+# select = driver.find_element_by_name("select")
+# search.click()
+# search.is_displayed()
+# search.submit()
+# search.send_keys("openSearch(True);")
+# QuickSearch = driver.find_element_by_xpath("//img[@title='Quick Search']")
+# search = driver.find_element_by_xpath("//img[@id='search']")
+# search = driver.find_element_by_xpath("//img[@name='search']")
+# search = driver.find_element_by_xpath("//img[@alt='Search']")
+# driver.switch_to.default_content()
 
-#driver.quit()
+# driver.quit()
 ## AdvSearhcPage = driver.page_source
 
 #   title = re.findall("<TITLE>.*</TITLE>",AdvSearhcPage)
 #   pprint(title)
 
-#FAILS!!!
-#driver.execute_script("goSearch();") #sumits the search, unfortunatly this then hangs!!!
-#driver.execute_script("_search()") unfortunatly this then hangs too!!!
-#driver.execute_script("parent.window.close();")
-#driver.execute_script("parent.TOOLBAR._submit();")
-#driver.execute_script("parent.LIST.goSearch()")
-#print("executeing...parent.TOOLBAR.goSearch()")
-#driver.execute_script("parent.TOOLBAR.goSearch()")
+# FAILS!!!
+# driver.execute_script("goSearch();") #sumits the search, unfortunatly this then hangs!!!
+# driver.execute_script("_search()") unfortunatly this then hangs too!!!
+# driver.execute_script("parent.window.close();")
+# driver.execute_script("parent.TOOLBAR._submit();")
+# driver.execute_script("parent.LIST.goSearch()")
+# print("executeing...parent.TOOLBAR.goSearch()")
+# driver.execute_script("parent.TOOLBAR.goSearch()")
 
-#td_tags[23][break-word]
-#soup = BeautifulSoup(driver.page_source,'lxml')
-#print(soup.prettify())
+# td_tags[23][break-word]
+# soup = BeautifulSoup(driver.page_source,'lxml')
+# print(soup.prettify())
 
-#find_the_first_date = soup.find('td', style = "WORD-WRAP: break-word", align = 'right')
-#find_the_dates = soup.find_all('td', style = "WORD-WRAP: break-word", align = 'right')
-#pprint(find_the_first_date)
-#pprint(find_the_dates)
+# find_the_first_date = soup.find('td', style = "WORD-WRAP: break-word", align = 'right')
+# find_the_dates = soup.find_all('td', style = "WORD-WRAP: break-word", align = 'right')
+# pprint(find_the_first_date)
+# pprint(find_the_dates)
